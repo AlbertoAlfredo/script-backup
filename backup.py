edd5e9962aabd46696480e23b3446640b932d1b3
Alberto Alfredo <raioalfredo@gmail.com>
2022-03-22 17:11:27 -0300

    colocado opção de compactar e do nome do arquivo de destino

--- backup.py
+++ backup.py
@@ -15,7 +15,7 @@ destino = data['destino'].replace("/", "\\")
 #destino = 'd:\\backup'
 #origem = "C:\\Versátil\\Versátile Expert\\dados_sposto.fdb"
 data_e_hora = datetime.now().strftime('%d-%m-%Y-%H%M%S')
-nome_do_arquivo = "dados_sposto.fdb"
+nome_do_arquivo = data['nome-arquivo-destino'] 
 if (data['usar-data-hora']):
     nome_arquivo_zip = data_e_hora+nome_do_arquivo
 else:
@@ -26,18 +26,21 @@ else:
 #shutil.move(".\\"+nome_arquivo_zip+".zip", destino)
 #print("Backup efetuado com sucesso \n")
 #input("Precione enter para finalizar..........")
-
+def mover(compactar = data['compactar'], origem = origem, destino = destino):
+    if(compactar):
+        shutil.make_archive(nome_arquivo_zip, 'zip','.\\', origem)
+        shutil.move(".\\"+nome_arquivo_zip+".zip", destino)
+    else:
+        shutil.copyfile(origem, destino)
 
 try:
     if os.path.exists(destino):
-        shutil.make_archive(nome_arquivo_zip, 'zip','.\\', origem)
-        shutil.move(".\\"+nome_arquivo_zip+".zip", destino)
+        mover()
         print("Backup efetuado com sucesso \n")
     else:
         try:
             os.mkdir(destino)
-            shutil.make_archive(nome_arquivo_zip, 'zip','.\\', origem)
-            shutil.move(".\\"+nome_arquivo_zip+".zip", destino)
+            mover()
         except:
             print("Erro ao criar o diretório de bakup, favor verificar o destino")
             input("Precione enter para finalizar..........")
